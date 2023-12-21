6a06dcf3a5658ea4e8b716d5a9a4be50d7638c34
erenyusufduran <eren.duran@colins.com>
2023-12-21 12:27:11 +0300

    products get food or cosmetic

--- src/api/controllers/product.controller.ts
+++ src/api/controllers/product.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from 'routing-controllers';
+import { Controller, Get, QueryParam, Res } from 'routing-controllers';
+import { Response } from 'express';
+
+import { ProductWithId } from '../../models';
+import { ProductService } from '../../services/product.service';
 
 @Controller('/product')
 export class ProductController {
-  
+  constructor(public productService: ProductService) {}
+
+  @Get()
+  public async getProducts(@Res() res: Response<Array<ProductWithId>>, @QueryParam('isFood') isFood?: boolean) {
+    try {
+      const mongoQuery = this.productService.mapAPIQueryParamForProducts(isFood);
+      const products = await this.productService.findAll(mongoQuery);
+      return res.status(200).send(products);
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,1 +1,16 @@
-export class ProductService {}
+import { Filter } from 'mongodb';
+
+import { ProductType, ProductWithId, Products } from '../models';
+
+export class ProductService {
+  findAll = async (query?: Filter<ProductWithId>): Promise<ProductWithId[]> => {
+    const products = await Products.find(query ?? {}).toArray();
+    return products;
+  };
+
+  mapAPIQueryParamForProducts = (isFood?: boolean): Filter<ProductWithId> => {
+    let mongoQuery: Filter<ProductWithId> = {};
+    mongoQuery = isFood ? { ...mongoQuery, productType: ProductType.F } : { ...mongoQuery, productType: ProductType.C };
+    return mongoQuery;
+  };
+}
